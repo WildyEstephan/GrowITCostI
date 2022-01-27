e2f99dbc44cdbf12296404756abd78ee9a9b3210
Wildy Estephan <westephan50@gmail.com>
2022-01-27 15:20:01 -0400

    Create code for destination cost by invoice

--- destination_cost_by_invoice/models/models.py
+++ destination_cost_by_invoice/models/models.py
@@ -1,18 +1,25 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
+class StockLandedCost(models.Model):
+    _inherit = 'stock.landed.cost'
 
-# class destination_cost_by_invoice(models.Model):
-#     _name = 'destination_cost_by_invoice.destination_cost_by_invoice'
-#     _description = 'destination_cost_by_invoice.destination_cost_by_invoice'
+    vendor_invoice_ids = fields.Many2many(
+        comodel_name='account.move',
+        string='Invoices', domain="[('state', 'not in', ('draft', 'cancel'))]")
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    def load_invoices(self):
+
+        for invoice in self.invoice_ids:
+
+            products = invoice.invoice_line_ids.filtered(lambda r: r.is_landed_costs_line)
+
+            for product in products:
+
+                self.env['stock.landed.cost.lines'].create({
+                    'cost_id': self.id,
+                    'account_id': product.account_id.id,
+                    'product_id': product.product_id.id,
+                    'price_unit': product.price_unit
+                })
